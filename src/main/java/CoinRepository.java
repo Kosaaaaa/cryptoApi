44a2cb4db5561eeff717df8f6432e2edb219e308
Kosaaaaa <43652764+Kosaaaaa@users.noreply.github.com>
2021-04-25 01:07:56 +0200

    create CoinRepository

---
+++ src/main/java/CoinRepository.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.OptionalDouble;
+
+public class CoinRepository {
+    private final List<Coin> coinList = new ArrayList<>();
+
+    public CoinRepository() {
+    }
+
+    public void addCoin(Coin coin) {
+        coinList.add(coin);
+    }
+
+    public double getAvgPrice() {
+        double avg = 0;
+        OptionalDouble avgOptional = coinList.stream().mapToDouble(Coin::getPriceUsd).average();
+        if (avgOptional.isPresent()) {
+            avg = avgOptional.getAsDouble();
+        }
+        return avg;
+    }
+
+
+    public String getHighestChangePercent() {
+        Coin coin = coinList.stream().max(Comparator.comparing(Coin::getChangePercent24Hr)).get();
+        return String.format("%s: %s", coin.getName(), coin.getChangePercent24Hr());
+    }
+
+    @Override
+    public String toString() {
+        return "CoinRepository{" +
+                "coinList=" + coinList +
+                '}';
+    }
+}
